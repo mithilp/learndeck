25d1ba8be26e021b27bafaf86b6b2e37061ccce6
mithilp <ping.mithil@gmail.com>
2024-01-20 11:53:23 -0800

    Course Thumbnails

---
+++ lib/components/thumbnail.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Thumbnail extends StatelessWidget {
+  final String image;
+  final String title;
+  final int progress;
+  final int totalUnits;
+  final String author;
+  final bool library;
+  final bool large;
+  const Thumbnail({super.key,required this.image, required this.title, required this.progress, required this.totalUnits, required this.author, required this.library, required this.large});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(children: <Widget>[
+      Container(
+        decoration: BoxDecoration(
+          color: const Color(0xff009966),
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: NetworkImage(
+                image),
+          ),
+          border: Border.all(
+            width: 8,
+            color: Color(0xff009966),
+          ),
+          borderRadius: BorderRadius.circular(25),
+        ),
+        height: large ? 275.0: 175.0,
+      ),
+      Container(
+        height: large ? 275.0: 175.0,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(
+              width: 8,
+              color: Color(0xff009966),
+            ),
+            borderRadius: BorderRadius.circular(25),
+            gradient: LinearGradient(
+                begin: FractionalOffset.topCenter,
+                end: FractionalOffset.bottomCenter,
+                colors: [
+                  Colors.grey.withOpacity(0.0),
+                  Colors.black,
+                ],
+                stops: [
+                  large? 0.5 : 0.1,
+                  1.0
+                ])),
+      ),
+      Positioned(
+        bottom: 20,
+        left: 20,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(title,
+                style: GoogleFonts.figtree(
+                  fontSize: 26,
+                  color: Colors.white,
+                  height: 1.15,
+                  fontWeight: FontWeight.w800,
+                )),
+            SizedBox(height: 5),
+            Text("${progress}/${totalUnits} units • @${author}",
+                style: GoogleFonts.figtree(
+                  fontSize: 18,
+                  color: Colors.white,
+                  height: 1.15,
+                  fontWeight: FontWeight.w800,
+                )),
+          ],
+        ),
+      ),
+      Positioned(
+        top: 20,
+        right: 20,
+        child: ElevatedButton(
+          onPressed: () {},
+          child: Text('Add${library ? 'ed' :''} to Library',
+              style: GoogleFonts.figtree(
+                fontSize: 18,
+                color: Colors.white,
+                height: 1.15,
+                fontWeight: FontWeight.w800,
+              )),
+          style: ElevatedButton.styleFrom(
+            primary: Color(0xff009966),
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12)),
+          ),
+        ),
+      ),
+    ]);
+  }
+}
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:auth0_flutter_platform_interface/src/user_profile.dart';
 import 'package:flutter/material.dart';
+import 'package:sample/components/thumbnail.dart';
 import 'package:sample/utils/models/user.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -11,46 +12,66 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     print(user);
     return Scaffold(
-        body: Center(
-        child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Row(
-            children: [
-              SizedBox(
-                width: 250,
-                child: Text('Ready to study, Mukunth?',
-                    style: GoogleFonts.figtree(
-                  fontSize: 32,
-                  color: Colors.black,
-                  height: 0.8,
-                  fontWeight: FontWeight.w800,
-                )),
+        body: SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
+        child: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                    width: 280,
+                    child: Text('Ready to study, Mukunth?',
+                        style: GoogleFonts.figtree(
+                          fontSize: 34,
+                          color: Colors.black,
+                          height: 1.15,
+                          fontWeight: FontWeight.w800,
+                        )),
+                  ),
+                  const Image(
+                    image: AssetImage('assets/logo.png'),
+                    height: 80,
+                  ),
+                ],
               ),
-              const Image(
-                image: AssetImage('assets/logo.png'),
-                height: 100,
-              ),
-            ],
-          ),
-        Container(
-          decoration: BoxDecoration(
-            color: const Color(0xff009966),
-            image: const DecorationImage(
-              image: NetworkImage('https://images.ctfassets.net/1aemqu6a6t65/5ZPPRFdVSbRyZVX4XyLmHv/db575493321930ff6849c8cdd7dd2472/Chinatown-4-Manhattan-NYC-Photo-Lucia-Vazquez.jpg'),
-              fit: BoxFit.cover,
-            ),
-            border: Border.all(
-              width: 8,
-              color: Color(0xff88888),
-            ),
-            borderRadius: BorderRadius.circular(12),
-          ),
-        )
-        ]
-          )
-        )
-    );
+              SizedBox(height: 20),
+                  Thumbnail(
+                    image:
+                    'https://images.ctfassets.net/1aemqu6a6t65/5ZPPRFdVSbRyZVX4XyLmHv/db575493321930ff6849c8cdd7dd2472/Chinatown-4-Manhattan-NYC-Photo-Lucia-Vazquez.jpg',
+                    title: 'The History of Chinatown',
+                    progress: 5,
+                    totalUnits: 12,
+                    author: 'mithilp',
+                    library: true,
+                    large: true,
+                  ),
+              SizedBox(height: 20),
+              Text('Your recent courses',
+                  style: GoogleFonts.figtree(
+                    fontSize: 34,
+                    color: Colors.black,
+                    height: 1.15,
+                    fontWeight: FontWeight.w800,
+                  )),
+                  SizedBox(height: 10),
+                  Thumbnail(
+                    image:
+                    'https://media.self.com/photos/5b6b0b0cbb7f036f7f5cbcfa/master/pass/apples.jpg',
+                    title: 'Types of Apples',
+                    progress: 6,
+                    totalUnits: 6,
+                    author: 'adeshmukh',
+                    library: false,
+                    large: false,
+                  ),
+
+            ])),
+      ),
+    ));
   }
 }
