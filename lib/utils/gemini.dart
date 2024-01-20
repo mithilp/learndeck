363893d61427f0d0e4afc5eaa8dff4a79995eb5c
bub655 <52846238+bub655@users.noreply.github.com>
2024-01-20 02:32:58 -0800

    gemini util text prompt

---
+++ lib/utils/gemini.dart
@@ -0,0 +1,53 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class GeminiAPI {
+  static Map<String, String> getHeader() {
+    return {
+      'Content-Type': 'application/json',
+    };
+  }
+
+  static Future<String> getGeminiData(message) async{
+    print('entered method');
+    var header = {
+      'Content-Type': 'application/json',
+    };
+    print('got header');
+    final Map<String, dynamic> requestBody = {
+      'contents': [
+        {
+          'parts': [
+            {
+              'text': message
+            }
+          ]
+        }
+      ],
+      'generationConfig': {
+        'temperature': 0.8,
+        'topK': 1,
+        'topP': 1,
+        'maxOutputTokens': 2048,
+        'stopSequences': []
+      }
+    };
+    print('sending request');
+    var response = await http.post(
+      Uri.parse('https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${dotenv.env['GEMINI_API']}'),
+      headers: header,
+      body: jsonEncode(requestBody),
+    );
+    if (response.statusCode == 200){
+      var jsonResponse = jsonDecode(response.body);
+      return jsonResponse.toString();
+    } else {
+      return 'Error 400: Request Failed';
+    }
+
+    return 'hello';
+  }
+}
