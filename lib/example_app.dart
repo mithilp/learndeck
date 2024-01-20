be96409ec3d754b7c10ac06ded20ff6ae916e2e3
mithilp <ping.mithil@gmail.com>
2024-01-20 00:40:11 -0800

    Remove unnecessary web auth

--- lib/example_app.dart
+++ lib/example_app.dart
@@ -1,5 +1,4 @@
 import 'package:auth0_flutter/auth0_flutter.dart';
-import 'package:auth0_flutter/auth0_flutter_web.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -20,29 +19,16 @@ class _ExampleAppState extends State<ExampleApp> {
   UserProfile? _user;
 
   late Auth0 auth0;
-  late Auth0Web auth0Web;
 
   @override
   void initState() {
     super.initState();
     auth0 = widget.auth0 ??
         Auth0(dotenv.env['AUTH0_DOMAIN']!, dotenv.env['AUTH0_CLIENT_ID']!);
-    auth0Web =
-        Auth0Web(dotenv.env['AUTH0_DOMAIN']!, dotenv.env['AUTH0_CLIENT_ID']!);
-
-    if (kIsWeb) {
-      auth0Web.onLoad().then((final credentials) => setState(() {
-            _user = credentials?.user;
-          }));
-    }
   }
 
   Future<void> login() async {
     try {
-      if (kIsWeb) {
-        return auth0Web.loginWithRedirect(redirectUrl: 'http://localhost:3000');
-      }
-
       var credentials = await auth0
           .webAuthentication(scheme: dotenv.env['AUTH0_CUSTOM_SCHEME'])
           .login();
@@ -57,16 +43,12 @@ class _ExampleAppState extends State<ExampleApp> {
 
   Future<void> logout() async {
     try {
-      if (kIsWeb) {
-        await auth0Web.logout(returnToUrl: 'http://localhost:3000');
-      } else {
-        await auth0
-            .webAuthentication(scheme: dotenv.env['AUTH0_CUSTOM_SCHEME'])
-            .logout();
-        setState(() {
-          _user = null;
-        });
-      }
+      await auth0
+          .webAuthentication(scheme: dotenv.env['AUTH0_CUSTOM_SCHEME'])
+          .logout();
+      setState(() {
+        _user = null;
+      });
     } catch (e) {
       print(e);
     }
